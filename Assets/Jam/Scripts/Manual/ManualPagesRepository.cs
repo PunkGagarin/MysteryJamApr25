260ee96912ac868f8ae9d9d3b544dc7829809ceb
madgine <marinabelogaz@gmail.com>
2025-05-04 17:23:19 +0300

    split pages states to prefabs;

--- Assets/Jam/Scripts/Manual/ManualPagesRepository.cs
+++ Assets/Jam/Scripts/Manual/ManualPagesRepository.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Jam.Scripts.GameplayData.Repositories;
+using Jam.Scripts.Manual;
 using UnityEngine;
 
 namespace Jam.Scripts.Manual
@@ -6,5 +8,12 @@ namespace Jam.Scripts.Manual
     [CreateAssetMenu(menuName = "Game Resources/Repositories/Manual Pages")]
     public class ManualPagesRepository : Repository<ManualPage>
     {
+        [field: SerializeField] public List<ManualPage> InitPages { get; private set; }
+        [field: SerializeField] public List<ManualPageList> Pages { get; private set; }
     }
+}
+[System.Serializable]
+public class ManualPageList
+{
+    public List<ManualPage> Pages;
 }
