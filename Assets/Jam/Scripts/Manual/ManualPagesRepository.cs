c0fead9513b4d224839fd4cd205e9772efeebd41
madgine <marinabelogaz@gmail.com>
2025-05-05 10:05:30 +0300

    Revert "split pages states to prefabs;"
    This reverts commit 260ee969

--- Assets/Jam/Scripts/Manual/ManualPagesRepository.cs
+++ Assets/Jam/Scripts/Manual/ManualPagesRepository.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using Jam.Scripts.GameplayData.Repositories;
-using Jam.Scripts.Manual;
 using UnityEngine;
 
 namespace Jam.Scripts.Manual
@@ -8,12 +6,5 @@ namespace Jam.Scripts.Manual
     [CreateAssetMenu(menuName = "Game Resources/Repositories/Manual Pages")]
     public class ManualPagesRepository : Repository<ManualPage>
     {
-        [field: SerializeField] public List<ManualPage> InitPages { get; private set; }
-        [field: SerializeField] public List<ManualPageList> Pages { get; private set; }
     }
-}
-[System.Serializable]
-public class ManualPageList
-{
-    public List<ManualPage> Pages;
 }
